911b3325df8717fe2839650622774f94c238ed2f
Nithiyasreek <nithiyasri28122005@gmail.com>
2026-07-04 10:24:59 +0530

    Time: 0 ms (100%), Space: 48.1 MB (50.34%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,23 +1,49 @@
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
-        int[] ans=new int[2];
-        int start=-1,end=-1,i;
-        int numsLength=nums.length;
-        for(i=0;i<nums.length;i++){
-            if(nums[i]==target){
-                start=i;
-                break;
-            }
+    int findfirstIndex(int[] arr,int target){
+    int low=0;
+    int high=arr.length-1;
+    while(low<=high){
+         int mid=(low+high)/2;
+        if(arr[mid]==target){
+          if(mid==0||arr[mid]!=arr[mid-1]){
+            return mid;
+          }
+            high=mid-1;
+          }
+          else if(arr[mid]<target){
+            low=mid+1;
+          }else{
+            high=mid-1;
+          }
         }
-            for(i=nums.length-1;i>=0;i--){
-                if(nums[i]==target){
-                    end=i;
-                    break;
-                }
-            }
-        ans[0]=start;
-        ans[1]=end;
+        return -1;
+    }
+    int findlastIndex(int[] arr, int target){
+         int low=0;
+         int high=arr.length-1;
+         while(low<=high){
+         int mid=(low+high)/2;
+        if(arr[mid]==target){
+          if(mid==arr.length-1||arr[mid]!=arr[mid+1]){
+            return mid;
+          }
+            low=mid+1;
+          }
+          else if(arr[mid]>target){
+            high=high-1;
+          }else{
+             low=mid+1;
+          }
+        }
+        return -1;
+       }
+    public int[] searchRange(int[] nums, int target) {
+        int startIndex=findfirstIndex(nums, target);//0(logN)
+        int lastIndex= findlastIndex(nums,target);
+        int[] ans=new int[2];
+        ans[0]=startIndex;
+        ans[1]=lastIndex;
         return ans;
-        
     }
 }
+        
